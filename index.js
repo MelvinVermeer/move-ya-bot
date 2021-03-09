eadf8346c9eef1500b06501e4ed52419257aa6c9
Melvin Vermeer <melvin.vermeer@gmail.com>
2021-03-09 23:01:26 +0100

    document first functionality

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const SlackBot = require('slackbots');
+const SlackBot = require("slackbots");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const bot = new SlackBot({
+  token: `${process.env.BOT_TOKEN}`,
+  name: "move-ya-bot",
+});
+
+bot.on("error", (err) => {
+  console.log(err);
+});
+
+bot.on("message", (data) => {
+  if (data.type === "message" && data.text.includes(bot.self.id)) {
+    bot.postMessage(data.channel, "Do 5 push ups", {
+      icon_emoji: ":robot_face:",
+    });
+    console.log(JSON.stringify(data, null, 2));
+  }
+});
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Move ya bot
+
+## Environment variables
+
+```
+BOT_TOKEN=xoxb-*****
+```
+
+## Current functionality
+
+When a message is posted that mentions the bot `@move-ya-bot`. The bot will repond with __"Do 5 push ups"__.
+
+## Resources
+
+- https://www.freecodecamp.org/news/building-a-slackbot-with-node-js-and-slackbots-js/
